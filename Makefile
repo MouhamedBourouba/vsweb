e4246641a5d7b2d1ce4db0b7a0380182b7beeb0d
MouhamedBourouba <mouhamedmobiledev@gmail.com>
2025-07-14 10:24:44 +0100

    http serve

--- Makefile
+++ Makefile
@@ -4,4 +4,7 @@ run:
 build:
 	@go build -o build/api cmd/api/main.go
 
+air:
+	@air --build.cmd "make build" --build.bin "build/api"
+
 .PHONY: build run
--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,5 +1,22 @@
 package main
 
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
 func main() {
-	println("hello world")
+	addr := ":8000"
+
+	http.HandleFunc("/api/compile", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodPost {
+		} else {
+			json.NewEncoder(w).Encode(map[string]any{"success": false, "message": "Method Not Allowed"})
+			w.WriteHeader(http.StatusBadRequest)
+		}
+	})
+
+	log.Println("Http Server started at", addr)
+	log.Fatal(http.ListenAndServe(addr, nil))
 }
