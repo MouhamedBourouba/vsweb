c3614d684caafddc58b1cde777efc61825f085a8
MouhamedBourouba <mouhamedmobiledev@gmail.com>
2025-07-25 18:42:10 +0100

    burrrrrrrrrrrrrrrrrrrrrrrrrrr

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,34 +1,87 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useRef } from 'react'
+import * as monaco from 'monaco-editor'
+import { create } from 'zustand'
+import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from './components/ui/resizable'
+
+interface EditorState {
+  editor: monaco.editor.IStandaloneCodeEditor | null
+  setEditor: (editor: monaco.editor.IStandaloneCodeEditor) => void
+}
+
+const useAppState = create<EditorState>()((set) => {
+  return {
+    editor: null,
+    setEditor: (editor: monaco.editor.IStandaloneCodeEditor) => { set({ editor: editor }) }
+  }
+})
+
+function Terminal() {
+  return (<div>Terminal</div>)
+}
+
+function Editor({ className = "", theme = "vs-dark" }: { className?: string, theme?: string }) {
+  const setEditor = useAppState((appState) => (appState.setEditor))
+  const containerRef = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    if (!containerRef.current) return
+
+    const editor = monaco.editor.create(containerRef.current, {
+      value: "// edit ur code here",
+      language: 'javascript',
+      autoIndent: 'advanced',
+      automaticLayout: true,
+      renderWhitespace: 'all',
+      renderLineHighlight: 'all',
+      minimap: {
+        scale: 10
+      },
+      tabSize: 2,
+      insertSpaces: true,
+      formatOnType: true,
+      formatOnPaste: true,
+      detectIndentation: true,
+      trimAutoWhitespace: true,
+      theme: theme,
+    })
+
+    setEditor(editor)
+
+    return () => {
+      editor.dispose()
+    }
+  }, [])
+
+  return <div className={className} ref={containerRef}></div>
+}
+
+function FileTree() {
+  return (<div>File tree</div>)
+}
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <ResizablePanelGroup direction="horizontal" className="h-screen w-screen">
+      <ResizablePanel defaultSize={20} minSize={10} maxSize={30}>
+        <FileTree />
+      </ResizablePanel>
+
+      <ResizableHandle />
+
+      <ResizablePanel defaultSize={80}>
+        <ResizablePanelGroup direction='vertical'>
+          <ResizablePanel>
+            <Editor className="h-full w-full" />
+          </ResizablePanel>
+          <ResizableHandle />
+          <ResizablePanel>
+            <Terminal />
+          </ResizablePanel>
+        </ResizablePanelGroup>
+      </ResizablePanel>
+
+
+    </ResizablePanelGroup>
   )
 }
 
